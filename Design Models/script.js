60852bd6dd867b10899bce101d1eda33adde7916
Adit Bala <adityarbala@gmail.com>
2020-08-14 22:57:51 -0400

    update select class model

--- Design Models/script.js
+++ Design Models/script.js
@@ -4,3 +4,11 @@ function openDialog() {
   dialog.open();
 }
 
+const MDCMenu = mdc.menu.MDCMenu;
+let menus = [...document.getElementsByClassName("mdc-menu")]
+menus = menus.map(menuElement => new MDCMenu(menuElement))
+function openMenu(index) {
+  const menu = menus[index]
+  menu.open = true;
+  menu.setFixedPosition(true);
+}
